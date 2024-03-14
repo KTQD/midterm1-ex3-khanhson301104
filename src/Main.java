efe33d3cb56fa02a756759600be856f5d6832911
khanhson301104 <52119462+khanhson301104@users.noreply.github.com>
2024-03-14 08:23:49 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            FileReader fileReader = new FileReader("input.txt");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
 
+            FileWriter fileWriter = new FileWriter("output.txt");
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
 
-        Bài làm viết tại đây
-
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String replacedLine = line.replace("Nha Trang", "Vũng Tàu");
+                bufferedWriter.write(replacedLine);
+                bufferedWriter.newLine();
+            }
+            bufferedReader.close();
+            bufferedWriter.close();
+            System.out.println("Da thay the");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
